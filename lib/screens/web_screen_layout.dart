0a1ffdf15d756178892a2c75ea74d07c108a214b
shahabcodes <shahabusaid.mm@gmail.com>
2023-01-07 21:27:11 +0300

    Displaying Web Chat App Bar

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -3,6 +3,8 @@ import 'package:whatsapp_ui/widgets/contacts_list.dart';
 import 'package:whatsapp_ui/widgets/web_profile_bar.dart';
 import 'package:whatsapp_ui/widgets/web_search_bar.dart';
 
+import '../widgets/web_chat_appbar.dart';
+
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
 
@@ -25,14 +27,20 @@ class WebScreenLayout extends StatelessWidget {
           ),
           // Web Screen
           Container(
-            width: MediaQuery.of(context).size.width * 0.75,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/backgroundImage.png'),
-                fit: BoxFit.cover,
+              width: MediaQuery.of(context).size.width * 0.75,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/backgroundImage.png'),
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-          )
+              child: Column(
+                children: [
+                  WebChatAppBar(),
+                  // Chat List
+                  // Message Input Box
+                ],
+              ))
         ],
       ),
     );
