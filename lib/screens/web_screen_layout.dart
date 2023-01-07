cb46dab0fee46cf6f84d30d14e27d4de42d01190
shahabcodes <shahabusaid.mm@gmail.com>
2023-01-07 17:59:07 +0300

    Displaying Web Profile Bar

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui/widgets/contacts_list.dart';
+import 'package:whatsapp_ui/widgets/web_profile_bar.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class WebScreenLayout extends StatelessWidget {
           child: SingleChildScrollView(
             child: Column(
               children: [
-                // Web Profile Bar
+                WebProfileBar(),
                 // Web Search Bar
                 ContactsList(),
               ],
