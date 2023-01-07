f7ebc6d12eb0c8d4f8381478d2df49b4dc0a3435
shahabcodes <shahabusaid.mm@gmail.com>
2023-01-07 20:52:46 +0300

    Displaying Web Search Bar

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui/widgets/contacts_list.dart';
 import 'package:whatsapp_ui/widgets/web_profile_bar.dart';
+import 'package:whatsapp_ui/widgets/web_search_bar.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
@@ -8,30 +9,32 @@ class WebScreenLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Row(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                WebProfileBar(),
-                // Web Search Bar
-                ContactsList(),
-              ],
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: const [
+                  WebProfileBar(),
+                  WebSearchBar(),
+                  ContactsList(),
+                ],
+              ),
             ),
           ),
-        ),
-        // Web Screen
-        Container(
-          width: MediaQuery.of(context).size.width * 0.75,
-          decoration: const BoxDecoration(
+          // Web Screen
+          Container(
+            width: MediaQuery.of(context).size.width * 0.75,
+            decoration: const BoxDecoration(
               image: DecorationImage(
-            image: AssetImage('assets/backgroundImage.png'),
-            fit: BoxFit.cover,
-          )),
-        )
-      ],
-    ));
+                image: AssetImage('assets/backgroundImage.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
